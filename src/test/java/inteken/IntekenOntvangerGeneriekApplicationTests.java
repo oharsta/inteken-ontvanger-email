2e48e35e4230ce940ffdbc613319a4cc8a4311b8
Okke Harsta <oharsta@zilverline.com>
2020-11-18 14:29:47 +0100

    first commit

---
+++ src/test/java/inteken/IntekenOntvangerGeneriekApplicationTests.java
@@ -0,0 +1,13 @@
+package org.surfnet.intekenontvangergeneriek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntekenOntvangerGeneriekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
